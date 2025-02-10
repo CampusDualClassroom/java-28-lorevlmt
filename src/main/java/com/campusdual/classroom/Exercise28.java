420b7c5954093447b533428320eab8deb1f5a967
lorevlmt <lore.vimontes@gmail.com>
2025-02-10 16:38:39 +0100

    Ejercicio 28 Corregido

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,13 @@
 package com.campusdual.classroom;
 
 public class Exercise28 {
-    public static void main(String[] args) throws Exception{
-        throw new Exception("Excepción lanzada desde el método main");
+    public static void main(String[] args) {
+
+        int dividendo = 3, divisor = 0;
+
+        int resultado = dividendo / divisor;
+
+        System.out.println(resultado);
+
     }
 }
